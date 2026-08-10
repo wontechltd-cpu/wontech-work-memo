d6e8c4e327c05d3a8dcdfd54a038a59fdf89916d
wontechltd-cpu <wontechltd@gmail.com>
2026-08-10 21:20:08 +0900

    Update preload.js

--- preload.js
+++ preload.js
@@ -2,6 +2,10 @@ const {contextBridge,ipcRenderer}=require('electron');
 contextBridge.exposeInMainWorld('desk',{
   top:v=>ipcRenderer.invoke('top',v),
   quoteWindow:open=>ipcRenderer.invoke('quote-window',open),
+  attachQuoteFile:(quoteId,oldStoredName)=>ipcRenderer.invoke('quote-attach-file',quoteId,oldStoredName),
+  openQuoteFile:storedName=>ipcRenderer.invoke('quote-open-file',storedName),
+  removeQuoteFile:storedName=>ipcRenderer.invoke('quote-remove-file',storedName),
+  exportQuotesExcel:(name,rows)=>ipcRenderer.invoke('quote-export-excel',name,rows),
   jpg:(name,html,options)=>ipcRenderer.invoke('jpg',name,html,options),
   pdf:(name,html,options)=>ipcRenderer.invoke('pdf',name,html,options),
   print:(name,html,options)=>ipcRenderer.invoke('print',name,html,options)
